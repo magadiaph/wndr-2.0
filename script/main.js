b45ee04a573f51d00fa4c6f88921ce940e61e65b
magadiaph <139517920+magadiaph@users.noreply.github.com>
2023-08-02 17:34:52 +0800

    cleaning of codes

--- script/main.js
+++ script/main.js
@@ -1,50 +1,9 @@
-// let number = document.querySelecto("number");
+
 let answer = document.querySelector("#answer");
 
 let number = document.querySelector("#number");
 let firstUnit = document.querySelector("#firstUnit");
 let secondUnit = document.querySelector("#secondUnit");
-
-// let hourAnswer = document.getElementById("hourAnswer");
-// let minuteAnswer = document.getElementById("minutesAnswer");
-// let secondsAnswer = document.getElementById("secondsAnswer");
-// {
-// function hour() {
-//     let numValue = parseFloat(number.value);
-//     let hourValue = numValue * 1;
-//     hourAnswer.innerHTML = "Hour/s: " + hourValue;
-
-//     let minuteValue = numValue * 60;
-//     minuteAnswer.innerHTML = "Minutes: " + minuteValue;
-
-//     let secondsValue = numValue * 3600;
-//     secondsAnswer.innerHTML = "Seconds: " + secondsValue;
-// }
-
-// function minutes() {
-//     let numValue = parseFloat(number.value);
-//     let hourValue = numValue / 60;
-//     hourAnswer.innerHTML = "Hour/s: " + hourValue;
-
-//     let minuteValue = numValue * 1;
-//     minuteAnswer.innerHTML = "Minutes: " + minuteValue;
-
-//     let secondsValue = numValue * 60;
-//     secondsAnswer.innerHTML = "Seconds: " + secondsValue;
-// }
-
-// function seconds() {
-//     let numValue = parseFloat(number.value);
-//     let hourValue = numValue / 3600;
-//     hourAnswer.innerHTML = "Hour/s: " + hourValue;
-
-//     let minuteValue = numValue / 60;
-//     minuteAnswer.innerHTML = "Minutes: " + minuteValue;
-
-//     let secondsValue = numValue * 1;
-//     secondsAnswer.innerHTML = "Seconds: " + secondsValue;
-// }
-// }
 
 function myDisplay () {
     let firstValue = firstUnit.value;
